34464faa5b7b1590ae35fefcb749321a43948e0a
Chia Ling <5076798+clinggit@users.noreply.github.com>
2019-06-12 17:57:53 -0700

    Added Import ChannelList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Chat, Channel, ChannelHeader, Thread, Window } from 'stream-chat-react';
+import { Chat, Channel, ChannelList, ChannelHeader, Thread, Window } from 'stream-chat-react';
 import { MessageList, MessageInput } from 'stream-chat-react';
 import { StreamChat } from 'stream-chat';
 
